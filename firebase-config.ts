f4d51de32f9cdee076f809748107e54ce5636b5d
Halyna Riabokonenko <sgallina89@gmail.com>
2024-03-18 10:01:46 +0100

    add types to firebase-config.ts, change Header styles

--- firebase-config.ts
+++ firebase-config.ts
@@ -3,7 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-export const firebaseConfig = {
+interface FirebaseConfigTypes {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
+
+export const firebaseConfig: FirebaseConfigTypes = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
