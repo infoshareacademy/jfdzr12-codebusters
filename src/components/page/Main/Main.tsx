05a7eafe9d3bff4e62d43964f2cba1cc25c359b1
Paula Markowska <paula.dziubek94@gmail.com>
2024-03-23 18:12:36 +0100

    added styles for Main and Paige Components

--- src/components/page/Main/Main.tsx
+++ src/components/page/Main/Main.tsx
@@ -6,9 +6,21 @@ export const Main = () => {
     return (
 <Page>
         
-        <h1 className={styles["headline-name"]}>Diary Daze</h1>
-        <p className={styles["normal-text"]}>keep a private online diary</p>
-        <img className={styles["styled-image"]} src="../../../public/images/main_banner.jpg" />
+        <h1 className={styles["headline-name"]}>
+            <span>D</span>
+            <span>i</span>
+            <span>a</span>
+            <span>r</span>
+            <span>y</span>
+            <span></span>
+            <span>D</span>
+            <span>a</span>
+            <span>z</span>
+            <span>e</span>
+            </h1>
+            <div className={styles["two-columns"]}>
+            <img className={styles["styled-image"]} src="Images/feather-pen.png" />
+            <p className={styles["handwriting-text"]}>keep a private online diary</p> </div>
         <div>
             <p className={styles["strong-text"]}>the safest place for your thoughts</p>
             <p className={styles["normal-text"]}>You want to keep your thoughts in a place where no one can find them? Or capture great ideas so they do not get lost? The online Diary Daze offers you a safe place for your very personal topics.</p>
