036dc2c26061036895d4b421ce1e1529e7b1e2a7
Maciej Kufel <mkufel43@gmail.com>
2024-03-23 18:59:48 +0100

    WIP footers socila media

--- src/components/structure/Footer/Footer.tsx
+++ src/components/structure/Footer/Footer.tsx
@@ -9,11 +9,33 @@ export const Footer = () => {
     const { mode, } = useContext(ModeContext);
 
     return (
-        <div className={classnames(styles["footer__container"], styles[mode])}>
-            <p className={classnames(styles["footer__text"], styles[mode])}>
-                Copyright &copy; 2024{" "}
-                <span className={styles["footer__text-company"]}>Diary-Daze</span>
-            </p>
+        <div className={classnames(styles["footer"], styles[mode])}>
+            <div className={classnames(styles["sb_footer section_padding"], styles[mode])}>
+                <div className={classnames(styles["sb_footer-links"], styles[mode])}>
+                    <div className={classnames(styles["sb_footer-links-div"], styles[mode])}>
+                        <h4>Company</h4>
+                        <a href="/about"></a>
+                        <p>About</p>
+                        <a href="/career"></a>
+                        <p>Career</p>
+                        <a href="/contact"></a>
+                        <p>Contact</p>
+                    </div>
+                    <div className={classnames(styles["sb_footer-links-div"], styles[mode])}>
+                        <h4>Partners</h4>
+                        <a href="/https://infoshareacademy.com/kursy/">Infoshare Academy</a>
+                    </div>
+                    <div className={classnames(styles["sb_footer-links-div"], styles[mode])}>
+                        <h4>Coming soon</h4>
+                        <div className={classnames(styles["socialmedia"], styles[mode])}>
+                            <p><img src={fb} alt="" /></p>
+                            <p><img src={insta} alt="" /></p>
+                            <p><img src={twitter} alt="" /></p>
+                            <p><img src={linkedin} alt="" /></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
