2247d1be7a8e5cd27443a40ec203411b5514d734
Maciej Kufel <mkufel43@gmail.com>
2024-03-24 20:47:00 +0100

    WIP Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import './App.css';
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import { Header } from './components/structure/Header/Header';
-<<<<<<< HEAD
-import { Main } from './components/page/Main/Main';
-import { Entry } from './components/page/Entry/Entry';
-=======
 import { Welcome } from './components/page/Welcome/Welcome';
+import { Home } from './components/page/Home/home';
 import { Entry } from './components/page/Entry/Entry'
->>>>>>> main
 import { Footer } from './components/structure/Footer/Footer';
 import { ModeProvider } from './providers/mode';
 import { useState, useEffect } from 'react';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { auth } from '../firebase-config';
-<<<<<<< HEAD
-import { Welcome } from './components/page/Home/home';
-=======
 import { NotFound } from './components/page/NotFound/NotFound';
->>>>>>> main
 
 function App(): JSX.Element {
   const [user, setUser] = useState<User | null>(null);
@@ -35,15 +27,10 @@ function App(): JSX.Element {
         <HashRouter>
           <Header user={user} />
           <Routes>
-<<<<<<< HEAD
-            <Route path="/" element={<Main />} />
-            <Route path="/entry" element={<Entry />} />
-            <Route path="/home" element={<Home />} />
-=======
             {!user && <Route path="/" element={<Welcome />} />}
             {user && <Route path="/entry" element={<Entry />} />}
+            {user && <Route path="/home" element={<Home />} />}
             <Route path="*" element={<NotFound />} />
->>>>>>> main
           </Routes>
           <Footer />
         </HashRouter>
--- src/components/page/Home/home.tsx
+++ src/components/page/Home/home.tsx
@@ -10,7 +10,7 @@ export const Home = () => {
     return (
         <Page>
             <div>
-                hello
+
             </div>
 
         </Page>
