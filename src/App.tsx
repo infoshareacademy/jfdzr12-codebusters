e83131e953a0543fc7140e24baad2b21e1188a11
Maciej Kufel <mkufel43@gmail.com>
2024-03-24 20:21:30 +0100

    WIP Welcome page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css';
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import { Header } from './components/structure/Header/Header';
 import { Main } from './components/page/Main/Main';
-import { Entry } from './components/page/Entry/Entry'
+import { Entry } from './components/page/Entry/Entry';
 import { Footer } from './components/structure/Footer/Footer';
 import { ModeProvider } from './providers/mode';
 import { useState, useEffect } from 'react';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { auth } from '../firebase-config';
+import { Welcome } from './components/page/Welcome/Welcome';
 
 function App(): JSX.Element {
   const [user, setUser] = useState<User | null>(null);
@@ -27,6 +28,7 @@ function App(): JSX.Element {
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/entry" element={<Entry />} />
+            <Route path="/welcome" element={<Welcome />} />
           </Routes>
           <Footer />
         </HashRouter>
---
+++ src/components/page/Welcome/Welcome.tsx
@@ -0,0 +1,18 @@
+import { Page } from "../../structure/Page/Page"
+// import { useContext } from "react";
+// import classnames from "classnames";
+// import styles from "./welcome.module.css";
+// import { ModeContext } from "@/providers/mode";
+
+export const Welcome = () => {
+    // const { mode, } = useContext(ModeContext);
+
+    return (
+        <Page>
+            <div>
+                hello
+            </div>
+
+        </Page>
+    )
+}
