f554aaad56887229505b80c999d2c4c2cd87d5d9
Halyna Riabokonenko <sgallina89@gmail.com>
2024-03-25 10:23:50 +0100

    add disabled styles to Button component

--- src/components/atomic/Button/Button.tsx
+++ src/components/atomic/Button/Button.tsx
@@ -6,14 +6,16 @@ import { ModeContext } from "@/providers/mode";
 interface ButtonProps {
     type?: "button" | "submit" | "reset";
     children: React.ReactNode;
+    disabled?: boolean;
 }
 
-export const Button = ({ type = "button", children }: ButtonProps) => {
+export const Button = ({ type = "button", children, disabled }: ButtonProps) => {
     const { mode } = useContext(ModeContext);
     return (
         <div className={classNames(
             styles["button-container"],
-            styles[mode]
+            styles[mode],
+            { [styles.disabled]: disabled }
         )}>
             <button className={classNames(
                 styles["button"],
