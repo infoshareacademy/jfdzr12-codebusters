67c99120f5bd83fee4a0c661c978eaa20a9f5cd0
Maciej Kufel <mkufel43@gmail.com>
2024-04-15 22:30:36 +0200

    WIP Contact us

--- src/components/page/Contact_Form/ContactForm.tsx
+++ src/components/page/Contact_Form/ContactForm.tsx
@@ -4,6 +4,8 @@ import { validateEmail } from "./validationEmail.js";
 import { useContext, useRef, useState } from "react";
 import { ModeContext } from "@/providers/mode";
 import { Button } from "@/components/ui/button.js";
+import { Page } from "@/components/structure/Page/Page";
+import { Headline } from "@/components/structure/Headline/Headline";
 
 export const ContactForm = () => {
     const [emailMessage, setEmailMessage] = useState("");
@@ -19,90 +21,90 @@ export const ContactForm = () => {
     };
 
     return (
-        <div
-            className={classnames(styles["contact-form__container"], styles[mode])}
-        >
-            <h1 className={classnames(styles["modal__header"], styles[mode])}>
-                Contact us
-            </h1>
-            <div className={classnames(styles["modal"])} id="contact">
-                <form
-                    action=""
-                    method="get"
-                    className={classnames(styles["modal__form"])}
-                    onSubmit={handleSubmit}
-                >
-                    <label
-                        htmlFor="title"
-                        className={classnames(styles["form__label"], styles[mode])}
+        <Page>
+            <Headline text="Contact Us"></Headline>
+            <div
+                className={classnames(styles["contact-form__container"], styles[mode])}
+            >
+                <div className={classnames(styles["modal"])} id="contact">
+                    <form
+                        action=""
+                        method="get"
+                        className={classnames(styles["modal__form"])}
+                        onSubmit={handleSubmit}
                     >
-                        Title
-                    </label>
-                    <input
-                        placeholder=""
-                        type="text"
-                        id="title"
-                        name="title"
-                        className={classnames(
-                            styles["form__input"],
-                            styles["form__input--text"],
-                            styles[mode]
-                        )}
-                        required
-                    />
-                    <label
-                        htmlFor="email"
-                        className={classnames(styles["form__label"], styles[mode])}
-                    >
-                        Email
-                    </label>
-                    <input
-                        ref={inputEmailEl}
-                        placeholder=""
-                        type="email"
-                        id="email"
-                        name="email"
-                        className={classnames(
-                            styles["form__input"],
-                            styles["form__input--email"],
-                            styles[mode]
-                        )}
-                        required
-                    />
-                    <p className={classnames(styles["form__error-message"])}>
-                        {emailMessage}
-                    </p>
-                    <label
-                        htmlFor="message"
-                        className={classnames(styles["form__label"], styles[mode])}
-                    >
-                        Message
-                    </label>
-                    <textarea
-                        placeholder=""
-                        id="message"
-                        className={classnames(
-                            styles["form__input"],
-                            styles["form__input--textarea"],
-                            styles[mode]
-                        )}
-                        minLength="20"
-                        name="message"
-                        rows="4"
-                        cols="50"
-                        required
-                    ></textarea>
-                    <Button
-                        type="submit"
-                        value="Send"
-                        className={classnames(
-                            styles["form__input"],
-                            styles["form__input--submit"],
-                            styles[mode]
-                        )}
-                    />
-                </form>
-            </div>
-        </div >
+                        <label
+                            htmlFor="title"
+                            className={classnames(styles["form__label"], styles[mode])}
+                        >
+                            Title
+                        </label>
+                        <input
+                            placeholder=""
+                            type="text"
+                            id="title"
+                            name="title"
+                            className={classnames(
+                                styles["form__input"],
+                                styles["form__input--text"],
+                                styles[mode]
+                            )}
+                            required
+                        />
+                        <label
+                            htmlFor="email"
+                            className={classnames(styles["form__label"], styles[mode])}
+                        >
+                            Email
+                        </label>
+                        <input
+                            ref={inputEmailEl}
+                            placeholder=""
+                            type="email"
+                            id="email"
+                            name="email"
+                            className={classnames(
+                                styles["form__input"],
+                                styles["form__input--email"],
+                                styles[mode]
+                            )}
+                            required
+                        />
+                        <p className={classnames(styles["form__error-message"])}>
+                            {emailMessage}
+                        </p>
+                        <label
+                            htmlFor="message"
+                            className={classnames(styles["form__label"], styles[mode])}
+                        >
+                            Message
+                        </label>
+                        <textarea
+                            placeholder=""
+                            id="message"
+                            className={classnames(
+                                styles["form__input"],
+                                styles["form__input--textarea"],
+                                styles[mode]
+                            )}
+                            minLength="20"
+                            name="message"
+                            rows="4"
+                            cols="50"
+                            required
+                        ></textarea>
+                        <Button
+                            type="submit"
+                            value="Send"
+                            className={classnames(
+                                styles["form__input"],
+                                styles["form__input--submit"],
+                                styles[mode]
+                            )}
+                        />
+                    </form>
+                </div>
+            </div >
+        </Page>
     );
 };
