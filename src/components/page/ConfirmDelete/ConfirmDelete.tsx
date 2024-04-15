9ad57fce59f1e749eb81ddcc7e4943703334da42
Halyna Riabokonenko <sgallina89@gmail.com>
2024-04-15 10:02:26 +0200

    change delete accout styles, image

--- src/components/page/ConfirmDelete/ConfirmDelete.tsx
+++ src/components/page/ConfirmDelete/ConfirmDelete.tsx
@@ -4,35 +4,46 @@ import { useContext } from "react";
 import styles from "./ConfirmDelete.module.css"
 import classNames from "classnames";
 import { ModeContext } from "@/providers/mode";
+import { Headline } from "@/components/structure/Headline/Headline";
 
 export const ConfirmDelete = () => {
     const { mode } = useContext(ModeContext);
 
     return (
         <Page>
+            <Headline text="Bye bye!"></Headline>
             <div className={classNames(
                 styles["confirm-delete__container"],
                 styles[mode]
             )}>
-            <Paper>
-            <div className={classNames(
-                styles["confirm-delete__container-text"],
-                styles[mode]
-            )}>
-                <h2 className={classNames(
-                styles["confirm-delete__text"],
-                styles[mode]
-            )}>Your account has been deleted</h2></div>
-            <div className={classNames(
-                styles["confirm-delete__container-img"],
-                styles[mode]
-            )}>
-{mode === "light" ? (
-                    <img className={styles["byeLight"]} src="images/icons/delete/goodbye.png" />) : 
-                    (<img className={styles["byeNight"]} src="images/icons/delete/goodbyeNight.png" />)}
-                    </div>
-            </Paper>
+
+                <div className={classNames(
+                    styles["confirm-delete__container-text"],
+                    styles[mode]
+                )}>
+                    <div className={classNames(
+                        styles["confirm-delete__text-container"],
+                        styles[mode]
+                    )}></div>
+                    <p className={classNames(
+                        styles["confirm-delete__text"],
+                        styles[mode]
+                    )}>Your account has been successfully deleted</p>
+                    <p className={classNames(
+                        styles["confirm-delete__text"],
+                        styles[mode]
+                    )}>We will miss you!</p>
                 </div>
+                <div className={classNames(
+                    styles["confirm-delete__container-img"],
+                    styles[mode]
+                )}>
+                    {mode === "light" ? (
+                        <img className={styles["byeLight"]} src="images/icons/delete/cry-cat-fish.gif" />) :
+                        (<img className={styles["byeNight"]} src="images/icons/delete/cry-cat-fish.gif" />)}
+                </div>
+
+            </div>
         </Page>
     )
 }
