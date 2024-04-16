a20193bda658c617400d71b847cdcaa00915f993
Halyna Riabokonenko <sgallina89@gmail.com>
2024-04-16 11:48:43 +0200

    add styles to Contact component

--- src/components/page/Contact/Contact.tsx
+++ src/components/page/Contact/Contact.tsx
@@ -7,9 +7,11 @@ import { Headline } from "@/components/structure/Headline/Headline";
 import { Button } from "@/components/atomic/Button/Button";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../../../firebase-config";
+import { ButtonTransparent } from "@/components/atomic/ButtonTransparent/ButtonTransparent";
 
 export const Contact: React.FC = () => {
-    const [message, setMessage] = useState<string | null>(null);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
     const { mode } = useContext(ModeContext);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -21,11 +23,10 @@ export const Contact: React.FC = () => {
         const message = formData.get('message') as string;
 
         if (!title.trim() || !email.trim() || !message.trim()) {
-            setMessage("Inputs cannot be empty");
+            setErrorMessage("Inputs cannot be empty");
             return;
         }
 
-        // Update this line to correct the collection path
         try {
             await addDoc(collection(db, 'contacts'), {
                 title,
@@ -33,80 +34,92 @@ export const Contact: React.FC = () => {
                 message,
                 timestamp: new Date()
             });
-            setMessage("Message sent successfully");
+            setSuccessMessage("Message sent successfully");
         } catch (error) {
             console.error("Error sending data: ", error);
-            setMessage("Error sending message. Please try again later.");
+            setErrorMessage("Error sending message. Please try again later.");
         }
     };
+
+    const handleCancel = () => {
+        console.log("Cancel");
+    }
 
     return (
         <Page>
             <Headline text="Contact Us" />
             <div className={classnames(styles["contact-form__container"], styles[mode])}>
-                <div className={classnames(styles["modal"])} id="contact">
-                    <form
-                        onSubmit={handleSubmit}
-                        id="contact-content__form"
-                        className={styles["contact-content__modal-form"]}
-                    >
-                        <label htmlFor="title" className={styles["contact-content__form-label"]}>
-                            Title
-                        </label>
-                        <input
-                            placeholder=""
-                            type="text"
-                            id="title"
-                            name="title"
-                            className={classnames(
-                                styles["contact-content__form-input"],
-                                styles["contact-content__form-input--text"],
-                                styles[mode]
-                            )}
-                            minLength={3}
-                            required
-                            aria-label="Title"
-                        />
-                        <label htmlFor="email" className={styles["contact-content__form-label"]}>
-                            Email
-                        </label>
-                        <input
-                            placeholder=""
-                            type="email"
-                            id="email"
-                            name="email"
-                            className={classnames(
-                                styles["contact-content__form-input"],
-                                styles["contact-content__form-input--email"],
-                                styles[mode]
-                            )}
-                            minLength={3}
-                            required
-                            aria-label="Email"
-                        />
-                        <label htmlFor="message" className={styles["contact-content__form-label"]}>
-                            Message
-                        </label>
-                        <textarea
-                            placeholder=""
-                            id="message"
-                            className={classnames(
-                                styles["contact-content__form-input"],
-                                styles["contact-content__form-input--textarea"],
-                                styles[mode]
-                            )}
-                            name="message"
-                            rows={4}
-                            cols={50}
-                            minLength={1}
-                            maxLength={500}
-                            required
-                            aria-label="Message"
-                        ></textarea>
-                        {message && <div>{message}</div>}
+                <form
+                    onSubmit={handleSubmit}
+                    id="contact-content__form"
+                    className={styles["contact-content__modal-form"]}
+                >
+                    <label htmlFor="title" className={styles["contact-content__form-label"]}>
+                        Title:
+                    </label>
+                    <input
+                        placeholder=""
+                        type="text"
+                        id="title"
+                        name="title"
+                        className={classnames(
+                            styles["contact-content__form-input"],
+                            styles["contact-content__form-input--text"],
+                            styles[mode]
+                        )}
+                        minLength={3}
+                        required
+                        aria-label="Title"
+                    />
+                    <label htmlFor="email" className={styles["contact-content__form-label"]}>
+                        Email:
+                    </label>
+                    <input
+                        placeholder=""
+                        type="email"
+                        id="email"
+                        name="email"
+                        className={classnames(
+                            styles["contact-content__form-input"],
+                            styles["contact-content__form-input--email"],
+                            styles[mode]
+                        )}
+                        minLength={3}
+                        required
+                        aria-label="Email"
+                    />
+                    <label htmlFor="message" className={styles["contact-content__form-label"]}>
+                        Message:
+                    </label>
+                    <textarea
+                        placeholder=""
+                        id="message"
+                        className={classnames(
+                            styles["contact-content__form-input"],
+                            styles["contact-content__form-input--textarea"],
+                            styles[mode]
+                        )}
+                        name="message"
+                        rows={4}
+                        cols={50}
+                        minLength={1}
+                        maxLength={500}
+                        required
+                        aria-label="Message"
+                    ></textarea>
+                    {errorMessage && <div className={classnames(
+                        styles["contact-form__error-message"],
+                        styles[mode]
+                    )}>{errorMessage}</div>}
+                    {successMessage && <div className={classnames(
+                        styles["contact-form__success-message"],
+                        styles[mode]
+                    )}>{successMessage}</div>}
+                    <div className={styles["contact-form__button-container"]}>
+                        <ButtonTransparent onClick={handleCancel}>Cancel</ButtonTransparent>
                         <Button>Send</Button>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </Page>
     );
