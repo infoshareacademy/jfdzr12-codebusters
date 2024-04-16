d3913cfdd42fa94d190981023129320845580692
Halyna Riabokonenko <sgallina89@gmail.com>
2024-04-16 10:23:24 +0200

    add types to Contact component

--- src/components/page/Contact/Contact.tsx
+++ src/components/page/Contact/Contact.tsx
@@ -1,33 +1,32 @@
+import React, { useContext, useRef, useState } from "react";
 import styles from "./Contact.module.css";
 import classnames from "classnames";
-import { validateEmail } from "./validationEmail.js";
-import { useContext, useRef, useState } from "react";
+import { validateEmail } from "./validationEmail";
 import { ModeContext } from "@/providers/mode";
 import { Page } from "@/components/structure/Page/Page";
 import { Headline } from "@/components/structure/Headline/Headline";
-import { Button } from "@/components/atomic/Button/Button.js";
+import { Button } from "@/components/atomic/Button/Button";
 
-export const ContactForm = () => {
-    const [emailMessage, setEmailMessage] = useState("");
-    const inputEmailEl = useRef(null);
+export const ContactForm: React.FC = () => {
+    const [emailMessage, setEmailMessage] = useState<string>("");
+
+    const inputEmailEl = useRef<HTMLInputElement>(null);
     const { mode } = useContext(ModeContext);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const emailValue = inputEmailEl.current.value;
+        const emailValue = inputEmailEl.current?.value ?? "";
 
-        setEmailMessage(validateEmail(emailValue));
+        setEmailMessage(validateEmail(emailValue) ?? "");
         console.log(emailValue);
     };
 
     return (
         <Page>
-            <Headline text="Contact Us"></Headline>
+            <Headline text="Contact Us" />
             <div className={classnames(styles["contact-form__container"], styles[mode])}>
                 <div className={classnames(styles["modal"])} id="contact">
                     <form
-                        action=""
-                        method="get"
                         className={classnames(styles["modal__form"])}
                         onSubmit={handleSubmit}
                     >
@@ -85,10 +84,10 @@ export const ContactForm = () => {
                                 styles["form__input--textarea"],
                                 styles[mode]
                             )}
-                            minLength="20"
+                            minLength={20}
                             name="message"
-                            rows="4"
-                            cols="50"
+                            rows={4}
+                            cols={50}
                             required
                         ></textarea>
                         <Button>Send</Button>
