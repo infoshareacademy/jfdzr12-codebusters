84a8489c81e20d20dc30bfd8c40b628b61eab785
Halyna Riabokonenko <sgallina89@gmail.com>
2024-04-21 17:15:19 +0200

    ButtonUp handler

--- src/components/atomic/ButtonUp/ButtonUp.tsx
+++ src/components/atomic/ButtonUp/ButtonUp.tsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
 import styles from "./ButtonUp.module.css";
+import classNames from "classnames";
 
 export const ButtonUp = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
     const handleUp = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
     };
 
+    const handleScroll = () => {
+        if (window.scrollY > 300) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+
     return (
-        <div onClick={handleUp} className={styles["button-back"]}>
-            <img src="/images/icons/arrows/up-button.png" alt="up icon" className={styles["button-back__image"]} />
+        <div onClick={handleUp} className={classNames(
+            styles["button-up"],
+            { [styles['show']]: isVisible, [styles['hide']]: !isVisible }
+        )}>
+            <img src="/images/icons/arrows/up-button.png" alt="up icon" className={styles["button-up__image"]} />
         </div>
     );
 };
