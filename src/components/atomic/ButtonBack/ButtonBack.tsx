42b3885f43a1e7da9d4b78042b59d20b6e7480fd
Halyna Riabokonenko <sgallina89@gmail.com>
2024-04-21 15:02:13 +0200

    use Button Back at all pages exclude '/'

--- src/components/atomic/ButtonBack/ButtonBack.tsx
+++ src/components/atomic/ButtonBack/ButtonBack.tsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./ButtonBack.module.css";
 
 export const ButtonBack = () => {
+    const navigate = useNavigate()
 
     const handleBack = () => {
-        window.history.back();
+        const previousPage = document.referrer;
+        if (previousPage && previousPage.includes(window.location.origin)) {
+            window.history.back();
+        } else {
+            navigate("/");
+        }
     }
 
     return (
