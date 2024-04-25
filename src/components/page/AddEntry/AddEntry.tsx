8d98ccf059546a9d650067713bb0c13524e33efb
Halyna Riabokonenko <sgallina89@gmail.com>
2024-04-25 22:28:26 +0200

    upload file

--- src/components/page/AddEntry/AddEntry.tsx
+++ src/components/page/AddEntry/AddEntry.tsx
@@ -3,7 +3,7 @@ import styles from "./AddEntry.module.css";
 import { useState } from "react";
 import classNames from "classnames";
 import { addDoc, collection } from "firebase/firestore";
-import { db } from "../../../../firebase-config";
+import { db, storage } from "../../../../firebase-config";
 import { User } from "firebase/auth";
 import { Button } from "@/components/atomic/Button/Button";
 import { Headline } from "@/components/structure/Headline/Headline";
@@ -12,6 +12,9 @@ import { ButtonTransparent } from "@/components/atomic/ButtonTransparent/ButtonT
 import { EntryArea } from "@/components/atomic/EntryArea/EntryArea";
 import { ButtonBack } from "@/components/atomic/ButtonBack/ButtonBack";
 import { useMode } from "@/providers/mode";
+import { DocumentReference, DocumentData } from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
+
 interface EntryProps {
     user: User | null;
 }
@@ -20,20 +23,18 @@ export const AddEntry = ({ user }: EntryProps) => {
     const { mode } = useMode();
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
     const [entryText, setEntryText] = useState<string>("");
+    const [photo, setPhoto] = useState<File | null>(null);
     const navigate = useNavigate();
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
-        const formData = new FormData(event.currentTarget);
-        const entry = formData.get('entry') as string;
 
         if (!user) {
             console.error('User is not authenticated');
             return;
         }
 
-        if (!entry.trim()) {
+        if (!entryText.trim()) {
             setErrorMessage("Entry cannot be empty");
             return;
         }
@@ -41,12 +42,20 @@ export const AddEntry = ({ user }: EntryProps) => {
         const userId = user.uid;
 
         try {
-            await addDoc(collection(db, `entries/${userId}/entry`), {
-                entry,
+            const docRef: DocumentReference<DocumentData> = await addDoc(collection(db, `entries/${userId}/entry`), {
+                entry: entryText,
                 email: user.email,
                 timestamp: new Date()
             });
+
+            if (photo) {
+                const photoRef = ref(storage, `photos/${userId}/${docRef.id}_photo`);
+
+                await uploadBytes(photoRef, photo);
+            }
+
             setEntryText("");
+            setPhoto(null);
             navigate("/")
         } catch (error) {
             console.log(error);
@@ -57,6 +66,14 @@ export const AddEntry = ({ user }: EntryProps) => {
     const handleReset = () => {
         setEntryText("");
         setErrorMessage(null);
+        setPhoto(null);
+    }
+
+    const handlePhotoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files && files.length > 0) {
+            setPhoto(files[0]);
+        }
     }
 
     return (
@@ -67,11 +84,9 @@ export const AddEntry = ({ user }: EntryProps) => {
                 styles[mode])
             }>
                 <Headline text="new entry" />
-                <form className={classNames(styles["entry__form"])}
-                    onSubmit={handleSubmit}>
-
-                    <EntryArea value={entryText} onChange={(e) => setEntryText(e.target.value)}>
-                    </EntryArea>
+                <form className={classNames(styles["entry__form"])} onSubmit={handleSubmit}>
+                    <EntryArea value={entryText} onChange={(e) => setEntryText(e.target.value)} />
+                    <input type="file" name="photo" accept="image/*" onChange={handlePhotoChange} />
                     {errorMessage && <div className={classNames(
                         styles["entry__error-message"],
                         styles[mode])}>{errorMessage}
